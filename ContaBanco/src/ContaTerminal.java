71332693a89b40991fc05c19a62d568bd92c26f0
liviaher <liviaher.oliveira@gmail.com>
2023-08-31 17:35:04 -0300

    classe conta terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Por favor, digite o número da conta:");
+    int numero = scanner.nextInt();
+
+    System.out.println("Por favor, digite o número da agência:");
+    String agencia = scanner.next();
+
+    System.out.println("Por favor, digite o nome do cliente:");
+    String nomeCliente = scanner.next();
+
+    System.out.println("Por favor, digite o saldo:");
+    Double saldo = scanner.nextDouble();
+
+    System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %2.2f já está disponível para saque", nomeCliente, agencia, numero, saldo);
+   }
 }
